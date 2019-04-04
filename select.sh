f8eac57b97ba57bdbffda101693422020a3d25cb
Susumu Seino <seino@susu.mu>
2019-04-04 13:00:01 +0900

    Tweak of select.sh

--- select.sh
+++ select.sh
@@ -4,8 +4,10 @@ JPG_DIR=$PJ_NAME"-jpg"
 RAW_DIR=$PJ_NAME"-raw"
 SELECT_DIR=$PJ_NAME"-select"
 
-pwd
+cd $SELECT_DIR
 for file in *.jpg *.JPG ; do
     [ -f "$file" ] || continue
-    # mv $file $JPG_DIR'/'
+     GET_NAME=`echo $file | sed 's/\.[^\.]*$//'`
+     cp "../"$RAW_DIR"/"$GET_NAME".ARW" "../"$SELECT_DIR"/"$GET_NAME".ARW"
+     echo "Done! Copy "$GET_NAME;
 done
